8109861a9bfd35609cea12df59287d5aa57959e7
fbuitrago <fabianbuitragov@gmail.com>
2023-03-07 23:13:22 -0500

    Add main html page and create window to open electron

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Electron App");
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
